a0db85861d5acb7ba46d55619ec76e2e8d1eb7d2
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-06-17 22:49:04 +0300

    feat: redux provider on app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { setupStore } from "./store/store.ts";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={setupStore}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
