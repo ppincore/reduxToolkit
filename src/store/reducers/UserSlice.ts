c6a16aefc0ff7e840a374d4e6fa5fe9bd67c4331
ppincore <liltracyn@yandex.ru>
2025-07-01 17:25:39 +0300

    feat: add extra reducers for slice

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,22 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { IUser } from "../../models/IUser";
+import { fetchUsersAsync } from "./ActionCreators";
 
 interface UserState {
   users: IUser[];
   isLoading: boolean;
-  error: string;
+  isError: string;
 }
 
 const initialState: UserState = {
   users: [],
   isLoading: false,
-  error: "",
+  isError: "",
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    // usersFetching(state) {
+    //   state.isLoading = true
+    // },
+    // usersFetchingSuccess(state, action:PayloadAction<IUser[]>) {
+    //   state.isLoading = false
+    //   state.users = action.payload
+    // },
+    // usersFetchingReject(state, action:PayloadAction<string>) {
+    //   state.isLoading = false
+    //   state.isError = action.payload
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsersAsync.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUsersAsync.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = action.error.message as string
+      })
+      .addCase(fetchUsersAsync.fulfilled, (state, action:PayloadAction<IUser[]>) => {
+        state.isLoading = false;
+        state.users = action.payload
+        state.isError = ''
+      });
+  },
 });
 
 export default userSlice.reducer;
