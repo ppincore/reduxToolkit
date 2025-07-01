23279b837f4d8eedc2fdbf50590b448f15075748
ppincore <liltracyn@yandex.ru>
2025-07-01 17:27:09 +0300

    feat: add post container in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import './App.css'
-import { useAppSelector } from './store/hooks/redux'
+import { useEffect } from "react";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./store/hooks/redux";
+import { userSlice } from "./store/reducers/UserSlice";
+import { fetchUsersAsync } from "./store/reducers/ActionCreators";
+import PostContainer from "./components/PostContainer";
+import PostContainer2 from "./components/PostContaine2";
 
 function App() {
-  const {} =useAppSelector(state => state.userReducer.)
+  const dispatch = useAppDispatch();
+  // const {users,isLoading,isError} =useAppSelector(state => state.userReducer)
+
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, []);
+
   return (
     <div>
-      цщкв
+      <div style={{ display: "flex" }}>
+        <PostContainer />
+        <PostContainer2 />
+      </div>
+      {/* {isLoading && <h1>Loading</h1>}
+      {isError && <h1>{isError}</h1>}
+      {JSON.stringify(users)} */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
